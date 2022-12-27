063fee1b9e8d5919e6a089e8abbdc454d4b7e632
Harsh chawla <harsh1560.be20@chitkara.edu.in>
2022-12-27 13:23:30 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const ReactDOMServer = require("react-dom/server");
-// const StaticRouter = require("react-router-dom/server");
 const mongoose = require('./data.js');
 const qroutes = require('./routes/quotes.js');
 const froutes = require('./routes/feedback.js');
 const path = require('path');
 const app = express()
-const port = process.env.PORT || 3000;
+
+const port = normalizePort (process.env.PORT || '3000');
 
 app.use(cors()); 
 app.use(express.static(path.join(__dirname+'/dist/build')));
 app.use(bodyParser.json());
-app.set('view engine', 'html');
 
 app.use('/quotes',qroutes);
 app.use('/feedbacks',froutes);
