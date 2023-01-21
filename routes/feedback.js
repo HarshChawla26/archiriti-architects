1746a987b815165f736ea6c837055d79f68c9a9f
Harsh chawla <harsh1560.be20@chitkara.edu.in>
2023-01-21 16:29:40 +0530

    added some feature

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const router  = express.Router();
-
+const nodemailer = require('nodemailer');
 
 const feedbackDB  = require('../models/feedback.js');
 
@@ -9,30 +9,69 @@ const feedbackDB  = require('../models/feedback.js');
 
 //get API
 router.get('/',(req,res)=>{
-    feedbackDB.find({})
+    feedbackDB.find()
     .then((e)=>{
-        res.send({message:"This is a feedback route"})
+        res.send({message:e})
     }).catch((err)=>{
         res.status(400).send({message:"Error occured"});
     })
 });
 
+
+async function sendmail(fdbk){
+    
+}
+
 //post API
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
     let fdbk = new feedbackDB({
         name : req.body.name,
         email : req.body.email,
         msg : req.body.msg
     });
-
-    fdbk.save((err,doc)=>{
+    await fdbk.save((err,doc)=>{
         if(err){
-            
-            res.status(400).send({message:`error occured : ${err}`});
-        }else{
-            res.status(200).send({message:"Data is posted"});
+            return res.status(400).send({message:`error occured : ${err}`});
         }
     });
+    
+    const transporter = await nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'harshchawla625@gmail.com',
+            pass: 'ebmjtnssizwbhagu'
+        }
+    });
+    
+    let info = transporter.sendMail({
+        from:'harshchawla625@gmail.com',
+        to:`${fdbk.email}`, 
+        subject:'Thank You for your feedback😊😊',
+        text:`Hello ${fdbk.name},
+        
+Thank you for your valuable feedback.
+
+We truly value your feedback and time you took to review us. Your feedback helps us improve our service for everyone.
+If you’d like to discuss how we could’ve made your experience better, please call us at +91-9416172811 or reply to this email.
+
+In case of any query we'll reply back to you within 24 hrs.
+
+We look forward to hearing from you again!😊
+
+Have a wonderful day!
+
+Kind regards,
+
+Ar. Jagdish chawla
+Archiriti architects
+Ambala Cantt.
+Haryana-133001
+`
+    }).then((info)=>{
+        return res.status(200).json({message:"Data is posted"})
+    }).catch(err=>{
+        return  res.status(500).json(err);
+    })
 });
 
 module.exports = router;
